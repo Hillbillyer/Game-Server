4bd9168eb67506aa095167a2a27156dfdf0d0228
Hillbillyer <contact@hillbillyer.net>
2023-10-01 21:10:40 +1100

    Testing.

--- server-installer.sh
+++ server-installer.sh
@@ -28,7 +28,7 @@ What server would you like to install?
         echo "Installing Dependencies"
         sleep 3s
         clear
-            sudo dpkg --add-architecture i386; sudo apt update; sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1-s1 lib32stdc++6 libsdl2-2.0-0:i386 steamcmd
+            sudo dpkg --add-architecture i386; sudo apt update; sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 libsdl2-2.0-0:i386 steamcmd
         sleep 3s
         clear
         echo "Installing CSGO Server"
